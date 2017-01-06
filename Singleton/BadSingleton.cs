6cbb82ce77fcbc8c8968053f7d6b13a7f10776fb
Abrar Ali <ali20011@hotmail.com>
2017-01-06 11:49:15 +0000

    Work?

---
+++ Singleton/BadSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class BadSingleton
+    {
+        private BadSingleton()
+        {
+        }
+
+        public static BadSingleton CreateInstance()
+        {
+            return new BadSingleton();
+        }
+        public void DoSomething()
+        {
+
+        }
+    }
+}
